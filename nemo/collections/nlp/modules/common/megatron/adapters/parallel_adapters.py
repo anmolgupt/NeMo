86c881095cd3fc88b6f409efde38243dbfba5cd5
Anmol Gupta <14880251+anmolgupt@users.noreply.github.com>
2024-04-08 15:00:45 -0700

    renamed grad reduce parameter

--- nemo/collections/nlp/modules/common/megatron/adapters/parallel_adapters.py
+++ nemo/collections/nlp/modules/common/megatron/adapters/parallel_adapters.py
@@ -168,9 +168,9 @@ class ParallelLinearAdapter(nn.Module, AdapterModuleUtil):
             model_parallel_config = ModelParallelConfig()
         self._sequence_parallel = model_parallel_config.sequence_parallel
         model_parallel_config.sequence_parallel = False  # SP is irrelevant for the lora linear layer
-        self.skip_reduce_in_bprop = False
+        self.disable_grad_reduce = False
         if self._sequence_parallel and not input_is_parallel:
-            self.skip_reduce_in_bprop = True
+            self.disable_grad_reduce = True
         if input_is_parallel:
             self.linear_in = RowParallelLinear(
                 in_features,
@@ -189,7 +189,7 @@ class ParallelLinearAdapter(nn.Module, AdapterModuleUtil):
                 bias=False,
                 gather_output=True,
                 init_method=self._get_init_fn(column_init_method),
-                skip_reduce_in_bprop=self.skip_reduce_in_bprop,
+                disable_grad_reduce=self.disable_grad_reduce,
             )
         if gather_output:
             self.linear_out = RowParallelLinear(
